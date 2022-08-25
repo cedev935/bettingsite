d8dd5cba004a480c52dc6d28b9bafc2cb105f931
cedev935 <cedev935@gmail.com>
2022-08-25 23:43:08 +0300

    add extra validation

--- app/Http/Controllers/Api/BetApiController.php
+++ app/Http/Controllers/Api/BetApiController.php
@@ -6,33 +6,41 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\PlaceBetApiRequest;
 use App\Models\Bet;
 use App\Models\BetSelection;
+use App\Models\Player;
+use App\Services\CustomValidator;
+use App\Services\OddsCalculator;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 
 class BetApiController extends Controller
 {
+    public function __construct(
+        OddsCalculator $oddsCalculator,
+    )
+    {
+        $this->OddsCalculator = $oddsCalculator;
+    }
+
     public function placeBet(PlaceBetApiRequest $request)
     {
+
+//        if (Player::find($request->player_id) === null) {
+//            Player::create([
+//                'name' => 'Random',
+//                'email' => 'Random',
+//                'password' => Random,
+//                'balance' => "1000"
+//
+//            ]);
+//        }
+
         $stake = Bet::create(["stake_amount" => $request->stake_amount]);
 
-        $selections = $request->selections;
-    // need to make service calculate max_win_amount
-        $odds = [];
-        foreach ($selections as $selection) {
-            $odds[] = $selection['odds'] ;
-        }
-        $product = 1;
-        foreach ($odds as $key => $value) {
-            $product *= $value;
-        }
-        dd($product);
+        $odds = $this->OddsCalculator->count($request);
 
-        foreach ($selections as $selection) {
-            BetSelection::create([
-                "selection_id" => $selection['id'],
-                "bet_id" => $stake->id,
-                "odds" => $selection['odds'],
-            ]);
+        if ($odds * floatval($request->stake_amount) > 20000) {
+            return response()->json(['message' => 'stake amount is too large']);
         }
 
         $validator = Validator::make($request->all(), [
@@ -42,7 +50,19 @@ class BetApiController extends Controller
 
         if ($validator->fails()) {
             $errors = $validator->errors();
-            return response()->json(['message' => $errors], 201);
+            return response()->json([
+                'selection_errors' => $errors
+            ], 201);
+        }
+
+        $selections = $request->selections;
+
+        foreach ($selections as $selection) {
+            BetSelection::create([
+                "selection_id" => $selection['id'],
+                "bet_id" => $stake->id,
+                "odds" => $selection['odds'],
+            ]);
         }
 
         return response()->json(['message' => 'Your bet is placed'], 201);
